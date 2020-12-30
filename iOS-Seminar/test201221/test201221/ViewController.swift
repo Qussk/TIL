c519a2ae159b754b0cf06dabae062e9ece2be5b2
Qussk <by1106007@gmail.com>
2020-12-30 20:15:37 +0900

    feat: add googleLogin

--- iOS-Seminar/test201221/test201221/ViewController.swift
+++ iOS-Seminar/test201221/test201221/ViewController.swift
@@ -6,16 +6,49 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
+
 
 class ViewController: UIViewController {
+  //MARK: -propertise
 
+  @IBOutlet weak var googleButton: GIDSignInButton!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
+    autoGoogleLogin()
     
   }
 
+  
+  //자동로그인
+  func autoGoogleLogin(){
+    GIDSignIn.sharedInstance()?.presentingViewController = self
+    
+  }
+  
+  
+  //MARK: - Action
+  //구글로그인 버튼
+  @IBAction func googleDidTap(_ sender: GIDSignInButton) {
+    GIDSignIn.sharedInstance().signIn()
+  }
+  
 
 }
 
+
+//MARK: -GIDSignInDelegate
+extension ViewController: GIDSignInDelegate {
+func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+  if let error = error {
+    return
+  }
+
+  guard let authentication = user.authentication else { return }
+  let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                    accessToken: authentication.accessToken)
+}
+
+}
