d91a0ba5fb937a5a37109903eef10a8d7222d693
Qussk <by1106007@gmail.com>
2021-01-18 19:51:19 +0900

    feat: add Analytics

--- iOS-Seminar-deligion/NewProject/NewProject/AppDelegate.swift
+++ iOS-Seminar-deligion/NewProject/NewProject/AppDelegate.swift
@@ -12,23 +12,23 @@ import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
-
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
-   
+    
     GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
     GIDSignIn.sharedInstance().delegate = self
     return true
   }
-
+  
   func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
     if let error = error {
       return
     }
-
+    
     guard let authentication = user.authentication else { return }
     let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                      accessToken: authentication.accessToken)
+                                                   accessToken: authentication.accessToken)
     
     //파이어베이스로 넘겨주는 부분
     Auth.auth().signIn(with: credential) {(user, error) in
@@ -37,26 +37,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
       }
     }
   }
-
+  
   
   
   func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
-      // Perform any operations when the user disconnects from app here.
-      // ...GIDSignIn.sharedInstance().handle(url)
+    // Perform any operations when the user disconnects from app here.
+    // ...GIDSignIn.sharedInstance().handle(url)
   }
-   
-
+  
+  
   
   
   //MARK: -handle
   @available(iOS 9.0, *)
   func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
-    -> Bool {
+  -> Bool {
     return GIDSignIn.sharedInstance().handle(url)
   }
   
   func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-      return GIDSignIn.sharedInstance().handle(url)
+    return GIDSignIn.sharedInstance().handle(url)
   }
   
   
@@ -64,19 +64,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
   
   
   // MARK: UISceneSession Lifecycle
-
+  
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
     // Called when a new scene session is being created.
     // Use this method to select a configuration to create the new scene with.
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
-
+  
   func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
   }
-
-
+  
+  
 }
 
